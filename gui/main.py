ab7b91f87147f594d5563312f70c063241d32a2a
Sivabalan10 <135865311+Sivabalan10@users.noreply.github.com>
2024-08-05 19:40:42 +0530

    Add files via upload

---
+++ gui/main.py
@@ -0,0 +1,10 @@
+from flask  import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>hi</h1>"
+
+if "__main__"== __name__:
+    app.run(debug=True,host='0.0.0.0',port=5005)
